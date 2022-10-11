4286f18424b97826302ef2b5f64b0cb1b260e8b8
sm1hero <sm1hero4ka@ya.ru>
2022-10-11 14:11:14 +0300

    Fix manifest

--- content.js
+++ content.js
@@ -1,1 +1,6 @@
+import connectFireBaseStorage from "./modules/fbconnect.js";
+
 console.log("Initial SCen");
+var firebase = connectFireBaseStorage();
+
+console.log(firebase);
---
+++ modules/fbconnect.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUMnjrvqFEwxSd5NdoQ1Gy88vbrFrLxl4",
+    authDomain: "scen-85df9.firebaseapp.com",
+    projectId: "scen-85df9",
+    storageBucket: "scen-85df9.appspot.com",
+    messagingSenderId: "125805843761",
+    appId: "1:125805843761:web:ebd9a81fa2598d775ccc10",
+    measurementId: "G-W4W56PV60D"
+};
+
+function connectFireBaseStorage() {
+    return getFirestore(initializeApp(firebaseConfig));
+}
+
+
+function getDocumentNodesFromBody() {
+    return document.body.childNodes;
+}
+
+function logData(data) {
+    console.log(data);
+}
+
+export default connectFireBaseStorage;
